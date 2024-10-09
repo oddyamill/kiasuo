14a9746e8d81ea49b8cdb630cb39e2d2781c69b8
oddyamill <43600905+oddyamill@users.noreply.github.com>
2024-10-09 21:47:00 +0700

    fix(scripts): registration script

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,1 +1,1 @@
-javascript:import(document.querySelector('script').src).then(i=>window.open('https://t.me/kiasuoRobot?start='+i.u().refreshToken))
+import(document.querySelector('script').src).then(i=>window.open('https://t.me/kiasuoRobot?start='+i.u().refreshToken))
