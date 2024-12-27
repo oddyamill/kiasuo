5f8e000c21697a1a03d4333003cc85807321be5e
oddyamill <43600905+oddyamill@users.noreply.github.com>
2024-12-27 17:39:11 +0700

    fix(worker): update config

--- cmd/worker/config.ts
+++ cmd/worker/config.ts
@@ -1,22 +1,12 @@
 export const ORIGIN_DOMAIN = "dnevnik.kiasuo.ru"
 
-export const CACHE_ROUTES = [
-	"/diary/api/user",
-	"/diary/api/recipients",
-	"/diary/api/study_periods",
-]
+export const CACHED_ROUTES = ["/diary/api/user", "/diary/api/recipients", "/diary/api/study_periods"]
 
 export const CACHE_HEADER = "Worker-Cache"
 
 export const AUTH_HEADER = "Worker-Authorization"
 
-export const EDGES = [
-	"DME",
-	"KLD",
-	"KJA",
-	"LED",
-	"SVX",
-]
+export const EDGES = ["DME", "KLD", "KJA", "LED", "SVX"]
 
 export const EDGE_HEADER = "Worker-Edge"
 
