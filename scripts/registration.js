c1bfb6a09a39666f382b80454dc444000f4b67f1
Roman <43600905+oddyamill@users.noreply.github.com>
2025-07-04 15:14:35 +0700

    chore: minify registration script

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,1 +1,1 @@
-import(document.querySelector('script').src).then(i=>window.open('https://t.me/kiasuoRobot?start='+i.u().refreshToken))
+import(document.scripts[0].src).then(i=>window.open('https://t.me/kiasuoRobot?start='+i.u().refreshToken))
